829858519e24cc6c3da7222e0bbe9395e8ca415c
ashebirch <bayochuchu1@gmail.com>
2023-02-18 23:44:34 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,15 @@
  */
 int main(void)
 {
-printfs("with proper grammar, but the outcome is a piece of art,\n");
+printfs("\"with proper grammar, but the outcome is a piece of art");
 return (0);
 }
+
+
+
+
+
+
+
+
+
