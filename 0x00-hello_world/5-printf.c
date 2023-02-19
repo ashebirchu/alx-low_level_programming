dbdcc4c5041ece2ccd7574a44bb01c28be3523af
ashebirch <bayochuchu1@gmail.com>
2023-02-18 22:58:42 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 /**
- *main - A program that prints a line with puts function
- * Return: 0 (Success)
+ *main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printfs("with proper grammar, but the outcome is a piece of art,\n")
+printfs("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
+
+
+
+
+
+
+
+
 
 
 
