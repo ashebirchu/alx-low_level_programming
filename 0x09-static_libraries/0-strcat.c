36c686c10a18936b1d2791290f797f0c61084972
ashebirch <bayochuchu1@gmail.com>
2023-03-29 04:54:56 -0700

    0-strcat

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,18 +1,43 @@
 #include "main.h"
-
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <ctype.h>
-
-#include <string.h>
-
+/**
+ * _strcat - fuction that concatenates two strings
+ * @dest: parameter defined in main
+ * @src: parameter defined in main
+ * Return: memory adress of function
+ */
 
 char *_strcat(char *dest, char *src)
 
 {
 
-    return strcat(dest, src);
+char *tmp = dest;
+
+while (*dest != '\0')
+
+{
+
+dest++;
+
+}
+
+while (*src != '\0')
+
+{
+
+*dest = *src;
+
+src++;
+
+dest++;
+
+}
+
+*dest++ = '\0';
+
+dest = tmp;
+
+return (dest);
 
 }
